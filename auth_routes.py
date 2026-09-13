1346dfdb52c6b2fd933136ad80ff5fad15a23ffa
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-09-12 23:57:37 -0300

    Add user account schemas and password hashing

--- auth_routes.py
+++ auth_routes.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
+from models import User
 
 
 auth_route = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @auth_route.get("/")
-def authenticate():
+async def home():
     """
     This is the default order route of our system. All order routes require authentication.
     """
@@ -13,3 +14,10 @@ def authenticate():
         "message": "You have accessed the default authentication route.",
         "authenticated": False
     }
+
+@auth_route.post('/create_account')
+async def create_account(
+    email: str,
+    password: str
+):
+    user = ...
